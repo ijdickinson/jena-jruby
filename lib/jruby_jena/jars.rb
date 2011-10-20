c2328dfc5baa6aded2863630abe8af4cb207d785
Ian Dickinson <i.j.dickinson@gmail.com>
2011-10-20 16:32:50 +0100

    changed to use consistent set of jars from TDB snapshot, to avoid classloader errors

--- lib/jruby_jena/jars.rb
+++ lib/jruby_jena/jars.rb
@@ -1,16 +1,19 @@
 %w[
-arq-2.8.7
+log4j-config
+arq-2.8.9-SNAPSHOT
+commons-codec-1.4
+httpclient-4.1.2
+httpcore-4.1.2
 icu4j-3.4.4
 iri-0.8
-jena-2.6.5-SNAPSHOT
-log4j-1.2.13
-lucene-core-2.9.3
-slf4j-api-1.5.6
-slf4j-log4j12-1.5.6
+jena-2.6.4
+log4j-1.2.16
+lucene-core-2.3.1
+slf4j-api-1.6.1
+slf4j-log4j12-1.6.1
 stax-api-1.0.1
 tdb-0.8.11-SNAPSHOT
 wstx-asl-3.2.9
 xercesImpl-2.7.1
-log4j-config
 ].each {|jar| require jar}
 
