9046209f3ff4021960b68b73c0e1eb74ab5a54e4
Ian Dickinson <i.j.dickinson@gmail.com>
2011-10-31 10:44:47 +0000

    Updated to use latest versions of Jena jars

--- lib/jruby_jena/jars.rb
+++ lib/jruby_jena/jars.rb
@@ -1,18 +1,19 @@
 %w[
-log4j-config
-arq-2.8.9-SNAPSHOT
-commons-codec-1.4
+commons-codec-1.5
+hamcrest-core-1.1
 httpclient-4.1.2
 httpcore-4.1.2
 icu4j-3.4.4
-iri-0.8
-jena-2.6.4
+jena-arq-2.8.9-incubating-SNAPSHOT
+jena-core-2.6.5-incubating-SNAPSHOT
+jena-iri-0.9.0-incubating-SNAPSHOT
+jena-tdb-0.9.0-incubating-SNAPSHOT
+junit-4.9
 log4j-1.2.16
-lucene-core-2.3.1
+log4j-config
 slf4j-api-1.6.1
 slf4j-log4j12-1.6.1
 stax-api-1.0.1
-tdb-0.9.0-SNAPSHOT
 wstx-asl-3.2.9
 xercesImpl-2.7.1
 ].each {|jar| require jar}
