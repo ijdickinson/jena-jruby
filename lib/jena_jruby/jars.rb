0baff9332e9892ebd4ff886bf9419193005cb37e
Ian Dickinson <ian@epimorphics.com>
2012-06-19 18:39:33 +0100

    Tracking latest Jena

--- lib/jena_jruby/jars.rb
+++ lib/jena_jruby/jars.rb
@@ -2,18 +2,20 @@
 # Use mvn dependency:tree to show deps from the pom.xml
 
 %w[
+commons-codec-1.5.jar
+hamcrest-core-1.1.jar
+httpclient-4.1.2.jar
+httpcore-4.1.3.jar
+junit-4.9.jar
 log4j-1.2.16.jar
 slf4j-api-1.6.4.jar
 slf4j-log4j12-1.6.4.jar
-commons-codec-1.5.jar
-httpclient-4.1.2.jar
-httpcore-4.1.3.jar
-jena-iri-0.9.1-SNAPSHOT.jar
 xercesImpl-2.10.0.jar
 xml-apis-1.4.01.jar
-jena-core-2.7.1-SNAPSHOT.jar
-jena-arq-2.9.1-SNAPSHOT.jar
-jena-tdb-0.9.1-SNAPSHOT.jar
+jena-iri-0.9.2-SNAPSHOT.jar
+jena-core-2.7.2-SNAPSHOT.jar
+jena-arq-2.9.2-SNAPSHOT.jar
+jena-tdb-0.9.2-SNAPSHOT.jar
 ].each do |jar|
   require jar
 end
