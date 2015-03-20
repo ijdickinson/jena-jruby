35409211375cf08844b95febddd4b2328faf2b94
Bruno Ferreira <chalkos@chalkos.net>
2015-03-20 11:51:41 +0000

    update jena

--- lib/jena_jruby/jars.rb
+++ lib/jena_jruby/jars.rb
@@ -1,26 +1,21 @@
-# Note: important to get these in reverse dependency order
-# Use mvn dependency:tree to show deps from the pom.xml
-
-%w[
-log4j-config.jar
-log4j-1.2.17.jar
-slf4j-log4j12-1.7.6.jar
-slf4j-api-1.7.6.jar
-xml-apis-1.4.01.jar
-xercesImpl-2.11.0.jar
-jena-iri-1.1.0.jar
-jena-core-2.12.0.jar
-jcl-over-slf4j-1.7.6.jar
-httpclient-cache-4.2.6.jar
-jackson-annotations-2.3.0.jar
-jackson-databind-2.3.3.jar
-jackson-core-2.3.3.jar
-jsonld-java-0.5.0.jar
-commons-codec-1.6.jar
-httpcore-4.2.5.jar
-httpclient-4.2.6.jar
-jena-arq-2.12.0.jar
-jena-tdb-1.1.0.jar
-].each do |jar|
-  require jar
-end
+require 'jackson-core-2.3.3'
+require 'httpclient-cache-4.2.6'
+require 'xercesImpl-2.11.0'
+require 'slf4j-log4j12-1.7.6'
+require 'jcl-over-slf4j-1.7.6'
+require 'httpcore-4.2.5'
+require 'log4j-1.2.17'
+require 'jena-iri-1.1.2'
+require 'commons-lang3-3.3.2'
+require 'jena-arq-2.13.0'
+require 'jackson-annotations-2.3.0'
+require 'libthrift-0.9.2'
+require 'commons-codec-1.6'
+require 'httpclient-4.2.6'
+require 'xml-apis-1.4.01'
+require 'slf4j-api-1.7.6'
+require 'jackson-databind-2.3.3'
+require 'jsonld-java-0.5.1'
+require 'commons-csv-1.0'
+require 'jena-core-2.13.0'
+require 'jena-tdb-1.1.2'
